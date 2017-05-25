1c0e39030c4eb6236e95ce3a2c0ddd0f0a3993cf
Cloud User <ec2-user@ip-172-31-13-222.us-west-2.compute.internal>
2017-05-25 10:44:45 -0400

    update .java file

--- src/main/java/com/sonatype/blog/App.java
+++ src/main/java/com/sonatype/blog/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Where there is a will there is a way!" );
+        System.out.println( "Whddddere there is a will there is a way!" );
     }
 }
