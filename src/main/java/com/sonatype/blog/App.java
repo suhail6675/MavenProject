f7c121937856678a56514571611af91f6933b8eb
Cloud User <ec2-user@ip-172-31-13-222.us-west-2.compute.internal>
2017-05-25 10:34:54 -0400

    update .java file

--- src/main/java/com/sonatype/blog/App.java
+++ src/main/java/com/sonatype/blog/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Where there is a will there is a way!" );
     }
 }
