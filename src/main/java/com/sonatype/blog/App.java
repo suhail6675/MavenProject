2948b3615e91d8eace3050fc03473b34e111243d
Suhail Lala <suhail@example.com>
2017-07-03 15:16:35 +0530

    Adding SOP to App.java file

--- src/main/java/com/sonatype/blog/App.java
+++ src/main/java/com/sonatype/blog/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Whddddere there is a will there is a way!" );
+	System.out.println("Welcome to the world of Java and Maven");
     }
 }
